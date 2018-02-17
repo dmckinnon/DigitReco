465a0e462b95970a17c61f7a04290fed4cf2c534
David McKinnon <damckinn@microsoft.com>
2018-02-17 12:16:24 -0800

    training done, now to add opencv wrapping

---
+++ classification.py
@@ -0,0 +1,21 @@
+import numpy as np
+import tensorflow as tf
+import cv2
+
+# Wanna take in an image
+# prep it for TF
+# use TF to classify it
+
+# Step 1: Given just an image of a digit,
+# dilate, threshold, prep image
+# Step 2: ruyn it through the classifier 
+
+
+def main():
+
+	""""""
+	print("3")
+
+
+if __name__ == "__main__":
+	main()
